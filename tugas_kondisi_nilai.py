a2fd6e983674244407ebe737ac6b40e1634cd21c
hermantoXYZ <andiupdates@gmail.com>
2025-03-04 09:49:05 +0800

    tugas saya dari ..

---
+++ tugas_kondisi_nilai.py
@@ -0,0 +1,25 @@
+#tugas 
+nama: BACO
+KELAS: 
+NIM: 
+
+# IF ELSE CONDITION
+// Task
+
+91 - 100 = A 
+86 - 90 = A- 
+81 - 85 = B+ 
+76 - 80 = B 
+71 - 75 = B- 
+66 - 70 = C+ 
+61 - 65 = C 
+56 - 60 = C- 
+51 - 55 = D+ 
+46 - 50 = D 
+41 - 45 = D- 
+0 - 41 = E
+
+coba buatkan program Nilai Lulus berdasarkan Indeks
+
+# IF ELSE CONDITION
+# Task
