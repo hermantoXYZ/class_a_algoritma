0696e75581f5510c63e73c674bcdcc5be62bed52
hermantoXYZ <andiupdates@gmail.com>
2025-03-04 09:46:41 +0800

    perkuliahan 4 maret 2025 class a

---
+++ A_4_maret.py
@@ -0,0 +1,87 @@
+
+# Menerima beberapa input dalam satu baris
+# nilai1, nilai2, nilai3 = input("Masukkan tiga nilai dipisahkan oleh spasi: ").split()
+
+# # Menampilkan input nilai
+# print("Nilai pertama: " + nilai1)
+# print("Nilai kedua: " + nilai2)
+# print("Nilai ketiga: " + nilai3)
+
+# Konversi Data dari List to Tuple
+
+# x= "Talita"
+# y=list(x)
+# print(y)
+
+
+# OPERASI ARITMATIKA
+# + (Penjumlahan)
+# - (Pengurangan)
+# * (Perkalian)
+# / (Pembagian: float)
+# // (Pembagian: integer)
+# % (Sisa Pembagian (Modulus)) 
+# ** (Pengkat)
+
+# # Praktik
+# x = 7
+# y = 2
+# print(x%y)
+
+
+# OPERASI PERBANDINGAN
+# x = 1000
+# y = 100
+
+# print(x<=y)
+
+
+# x = 5
+# print(bin(x))
+
+
+# OPERASI IDENTITAS
+# a = [1, 2, 3]
+# b = a
+# c = [1, 2, 3]
+
+# print(a is b)   # True (b adalah alias dari a)
+# print(a is c)   # False (walaupun isinya sama, ID objek berbeda)
+# print(a is not c) # True
+
+
+# OPERASI KEANGGOTAAN
+# list_angka = [1, 2, 3, 4, 5]
+
+# print(3 in list_angka)     # True
+# print(6 not in list_angka) # True
+
+# # Operasi keanggotaan pada string
+
+# list_number = [100, 200, 384, 4049]
+# print( 100 in list_number)
+
+fruits = ['apple', 'banana', 'orange', 'grape']
+
+# Memeriksa apakah 'banana' ada dalam list
+if 'es buah' not in fruits:
+    print("belum watktunya berbuka.")
+
+# Memeriksa apakah 'pear' tidak ada dalam list
+if 'pear' not in fruits:
+    print("Pear tidak ada dalam list buah-buahan.")
+
+
+a=10
+if(a>10):
+    print("Value of a is greater than 10")
+else :
+    print("Value of a is 10")
+
+
+# STATMENT 1
+JIKA A LEBIH BESAR DARI 10
+OUPUT: nilai lebih dari besar 10
+
+# STATMENT 2
+OUPUT: nilai lebih kecil dari 10
---
+++ fungsi.py
@@ -0,0 +1,19 @@
+
+x = 10
+y = 3.14
+z = "Hello"
+a = True
+b = [1, 2, 3]
+c = (4, 5, 6)
+d = {7, 8, 9}
+e = {"nama": "Budi", "usia": 25}
+
+print(type(x))  # Output: <class 'int'>
+print(type(y))  # Output: <class 'float'>
+print(type(z))  # Output: <class 'str'>
+print(type(a))  # Output: <class 'bool'>
+print(type(b))  # Output: <class 'list'>
+print(type(c))  # Output: <class 'tuple'>
+print(type(d))  # Output: <class 'set'>
+print(type(e))  # Output: <class 'dict'>
+print(e["nama"])
