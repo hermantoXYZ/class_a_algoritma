b9950e3efd8b6e89a1e1d748afc640bbe6857ce6
hermantoXYZ <andiupdates@gmail.com>
2025-03-10 11:19:45 +0800

    Kelas B

---
+++ B_10_maret.py
@@ -0,0 +1,40 @@
+# a = 10
+# b = 5
+
+# hasil_sama_dengan = a == b
+# hasil_tidak_sama_dengan = a != b
+# hasil_lebih_besar_dari = a > b
+# hasil_lebih_kecil_dari = a < b
+# hasil_lebih_besar_sama_dengan = a >= b
+# hasil_lebih_kecil_sama_dengan = a <= b
+
+# print("Sama dengan:", hasil_sama_dengan)  # False
+# print("Tidak sama dengan:", hasil_tidak_sama_dengan)  # True
+# print("Lebih besar dari:", hasil_lebih_besar_dari)  # True
+# print("Lebih kecil dari:", hasil_lebih_kecil_dari)  # False
+# print("Lebih besar atau sama dengan:", hasil_lebih_besar_sama_dengan)  # True
+# print("Lebih kecil atau sama dengan:", hasil_lebih_kecil_sama_dengan)  # False
+
+# fruits = ['apple', 'banana', 'orange', 'grape', 'es buah']
+
+# # Memeriksa apakah 'banana' ada dalam list
+# if 'es buah' in fruits:
+#     print("es buah ada dalam list buah-buahan.")
+
+# # Memeriksa apakah 'pear' tidak ada dalam list
+# if 'jalangkote' not in fruits:
+#     print("Jalan kOTE ada dalam list buah-buahan.")
+
+#koding01 kondisi if else
+# x = int(input('masukkan angka:'))  
+# Y = int(input('masukkan angka:'))
+
+
+# if (y >80):
+#     print("Anda lulus")
+# else:
+#     print("coba lagi")
+    
+
+for i in range(20):
+    print("Belajar Loop:", i)
--- fungsi.py
+++ fungsi.py
@@ -1,19 +1,29 @@
 
-x = 10
-y = 3.14
-z = "Hello"
-a = True
-b = [1, 2, 3]
-c = (4, 5, 6)
-d = {7, 8, 9}
-e = {"nama": "Budi", "usia": 25}
+# x = 10
+# y = 3.14
+# z = "Hello"
+# a = True
+# b = [1, 2, 3]
+# c = (4, 5, 6)
+# d = {7, 8, 9}
+# e = {"nama": "Budi", "usia": 25}
 
-print(type(x))  # Output: <class 'int'>
-print(type(y))  # Output: <class 'float'>
-print(type(z))  # Output: <class 'str'>
-print(type(a))  # Output: <class 'bool'>
-print(type(b))  # Output: <class 'list'>
-print(type(c))  # Output: <class 'tuple'>
-print(type(d))  # Output: <class 'set'>
-print(type(e))  # Output: <class 'dict'>
-print(e["nama"])
+# print(type(x))  # Output: <class 'int'>
+# print(type(y))  # Output: <class 'float'>
+# print(type(z))  # Output: <class 'str'>
+# print(type(a))  # Output: <class 'bool'>
+# print(type(b))  # Output: <class 'list'>
+# print(type(c))  # Output: <class 'tuple'>
+# print(type(d))  # Output: <class 'set'>
+# print(type(e))  # Output: <class 'dict'>
+# print(e["nama"])
+
+fruits = ['apple', 'banana', 'orange', 'grape']
+
+# Memeriksa apakah 'banana' ada dalam list
+if 'banana' in fruits:
+    print("Banana ada dalam list buah-buahan.")
+
+# Memeriksa apakah 'pear' tidak ada dalam list
+if 'pear' not in fruits:
+    print("Pear tidak ada dalam list buah-buahan.")
